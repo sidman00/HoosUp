267285977d6e02141c738050caecb6505d59b458
Siddharth Ghatti <sg4ff@virginia.edu>
2019-03-02 12:44:54 -0500

    Getting app ready for heroku deployment

--- HoosUp/settings.py
+++ HoosUp/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
