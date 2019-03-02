79747d56fb728c4973dbbe736d8b0f979f87e532
Siddharth Ghatti <sg4ff@virginia.edu>
2019-03-02 17:06:30 -0500

    Added middleware to check whose online
    tag:
    {{ request.online_now }}

--- HoosUp/settings.py
+++ HoosUp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'HoosUp.middleware.OnlineNowMiddleware',
 ]
 
 ROOT_URLCONF = 'HoosUp.urls'
